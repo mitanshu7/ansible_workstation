12947be72ad42de7c298a32705c04ce78fa1444b
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2024-06-07 04:53:50 +0530

    added pipx as the ansible installation methon in run_setup.sh, need to rectify tweaks.yml on debian

--- run_setup.sh
+++ run_setup.sh
@@ -5,11 +5,17 @@ install_ansible() {
   if [ "$(command -v apt)" ]; then
     echo "Installing Ansible on Ubuntu/Debian..."
     sudo apt update
-    sudo apt install -y ansible
+    python3 -m pip install --user pipx
+    python3 -m pipx ensurepath
+    sudo pipx ensurepath --global
+    pipx install --include-deps ansible
   elif [ "$(command -v dnf)" ]; then
     echo "Installing Ansible on Fedora/RedHat..."
     sudo dnf update
-    sudo dnf install -y ansible
+    sudo dnf install -y pipx
+    pipx ensurepath
+    sudo pipx ensurepath --global
+    pipx install --include-deps ansible
   else
     echo "This script only supports Ubuntu/Debian and Fedora/RedHat based systems."
     exit 1
@@ -20,6 +26,9 @@ install_ansible() {
 if ! hash ansible 2>/dev/null; then
   install_ansible
 fi
+
+# To update path
+source ~/.bashrc
 
 echo "Ansible installation complete!"
 
